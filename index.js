5294a0c591bf332eec7cd66dfaeaa77d70a2edee
andreas <andreas@wellenvogel.de>
2021-01-02 12:46:38 +0100

    use colormap, adapt time format

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 console.log("history main loaded");
 (function(){
+    let COLORMAP=["#000000", "#FFFF00", "#1CE6FF", "#FF34FF",
+        "#FF4A46", "#008941", "#006FA6", "#A30059",
+        "#FFDBE5", "#7a4900", "#0000A6", "#63FFAC",
+        "#B79762", "#004D43", "#8FB0FF", "#997D87",
+        "#5A0007", "#809693", "#FEFFE6", "#1B4400",
+        "#4FC601", "#3B5DFF", "#4A3B53", "#FF2F80",
+        "#61615A", "#BA0900", "#6B7900", "#00C2A0",
+        "#FFAA92", "#FF90C9", "#B903AA", "#D16100",
+        "#DDEFFF", "#000035", "#7B4F4B", "#A1C299",
+        "#300018", "#0AA6D8", "#013349", "#00846F",
+        "#372101", "#FFB500", "#C2FFED", "#A079BF",
+        "#CC0744", "#C0B9B2", "#C2FF99", "#001E09",
+        "#00489C", "#6F0062", "#0CBD66", "#EEC3FF",
+        "#456D75", "#B77B68", "#7A87A1", "#788D66",
+        "#885578", "#FAD09F", "#FF8A9A", "#D157A0",
+        "#BEC459", "#456648", "#0086ED", "#886F4C",
+        "#34362D", "#B4A8BD", "#00A6AA", "#452C2C",
+        "#636375", "#A3C8C9", "#FF913F", "#938A81",
+        "#575329", "#00FECF", "#B05B6F", "#8CD0FF",
+        "#3B9700", "#04F757", "#C8A1A1", "#1E6E00",
+        "#7900D7", "#A77500", "#6367A9", "#A05837",
+        "#6B002C", "#772600", "#D790FF", "#9B9700",
+        "#549E79", "#FFF69F", "#201625", "#72418F",
+        "#BC23FF", "#99ADC0", "#3A2465", "#922329",
+        "#5B4534", "#FDE8DC", "#404E55", "#0089A3",
+        "#CB7E98", "#A4E804", "#324E72", "#6A3A4C"];
     function nextColor(current){
         let r=parseInt(current.substr(1,2),16);
         let g=parseInt(current.substr(3,2),16);
@@ -9,6 +35,7 @@ console.log("history main loaded");
         b=parseInt(256*Math.random()+b)%256;
         return '#'+r.toString(16)+g.toString(16)+b.toString(16);
     }
+
     function removeChart(){
         let chart=document.getElementById('chart');
         chart.innerHTML="";
@@ -50,7 +77,9 @@ console.log("history main loaded");
                 .range([addLeft,width]);
         svg.append("g")
             .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(x));
+            .call(d3.axisBottom(x)
+                .tickFormat(d3.timeFormat("%d/%Hh"))
+            );
         let currentY;
         let leftMargin=0;
         for (let idx=0;idx<fields.length;idx++) {
@@ -164,12 +193,13 @@ console.log("history main loaded");
                         fillChart();
                     })
                 }
-                let color='#000000';
+                let colorIndex=4;
                 if (data.fields){
                     let selectorList=document.getElementById('selectors');
                     for (let i=0;i<data.fields.length;i++){
-                        let fs=createFieldSelector(data.fields[i],color,"fieldSelector")
-                        color=nextColor(color);
+                        let fs=createFieldSelector(data.fields[i],COLORMAP[colorIndex],"fieldSelector")
+                        colorIndex+=1;
+                        if (colorIndex >= COLORMAP.length) colorIndex=0;
                         selectorList.appendChild(fs);
                     }
                 }
