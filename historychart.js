6bb2ab1dfcc2cb15709a5c1b23fdc61106f3202e
pozoo <46608416+pozoo@users.noreply.github.com>
2025-09-25 20:57:30 +0200

    reduced number of x-axis ticks for readability on mobile

--- historychart.js
+++ historychart.js
@@ -224,7 +224,8 @@ console.log("history diagram loaded");
         svg.append("g")
             .attr("transform", "translate(0," + height + ")")
             .call(d3.axisBottom(this.xScale)
-                .tickFormat(d3.timeFormat("%d/%H:%M"))
+            .ticks(6)
+            .tickFormat(d3.timeFormat("%d/%H:%M"))
             );
         let currentY;
         let leftMargin=0;
